c3b098aea7ec9acbd93c738b969796bb8c167988
Vikil Naik <vikilnaik@Vikils-MBP.hitronhub.home>
2021-01-04 18:22:35 -0500

    Making changes to the mobile responsiveness of the website

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ function animateSlides() {
 		slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
 		slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
 		slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
-		slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0,5");
 		// Create scene
 		slideScene = new ScrollMagic.Scene({
 			triggerElement: slide,
@@ -118,12 +117,6 @@ barba.init({
 			beforeEnter() {
 				logo.href = "../index.html";
 				detailAnimation();
-				gsap.fromTo(
-					".nav-header",
-					1,
-					{ y: "100%" },
-					{ y: "0%", ease: "power2.inOuts" }
-				);
 			},
 			beforeLeave() {
 				controller.destroy();
@@ -163,6 +156,13 @@ barba.init({
 					{ x: "100%", stagger: 0.25, onComplete: done }
 				);
 				tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+				tl.fromTo(
+					".nav-header",
+					1,
+					{ y: "-100%" },
+					{ y: "0%", ease: "power2.inOuts" },
+					"-=1.5"
+				);
 			},
 		},
 	],
